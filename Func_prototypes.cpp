bed98fe24d857309e5f149ecb36c5084610e88bf
Riti <112542247+sinhariti@users.noreply.github.com>
2022-11-28 09:45:57 +0530

    Create Func_prototypes.cpp

---
+++ Func_prototypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+//Function prototype
+//lets the complier know about function written after the main() func beforehand 
+int sum (int a, int b); //--acceptable 
+int sum(int a, b);//--unacceptable 
+int sum (int, int);//accpetable ie no need to put arguments 
+
+int main(){
+    int n1,n2;
+    cout<<"Enter the first number :"<<endl;
+    cin>>n1;
+    cout<<"Enter the second number :"<<endl;
+    cin>>n2;
+    cout<<"The sum is :"<<sum(n1,n2);
+    return 0;
+}
+int sum(int a,int b){
+    int c=(a+b);
+    return c;
+}
