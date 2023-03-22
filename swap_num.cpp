048c8401e60b4d2835fe7a6b56733fb20ff6b40e
Riti <112542247+sinhariti@users.noreply.github.com>
2023-03-22 21:28:29 +0530

    Create swap_num.cpp

---
+++ swap_num.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    //swap numbers
+    int a=4,b=5;
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    cout<<a<<" "<<b;
+    
+}
